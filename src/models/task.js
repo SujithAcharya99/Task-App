1c7e8d1411ee896bf80ca230da2a5f0aa62eaf99
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-18 20:06:20 +0530

    Update task.js

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -11,7 +10,28 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         required: true,
         default : false
-    }
-})
+    },
+    owner: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true,
+        ref: 'User'
+    }  
+}, {
+    timestamps: true
+});
+
+
+const Task = mongoose.model('Task', taskSchema)
+
+// const task = new Task({
+//     description: 'Task-app',
+//     completed: true
+// });
+
+// task.save().then(() => {
+//     console.log(task);
+// }).catch((error) => {
+//     console.log(error);
+// });
 
 module.exports = Task;
