ebc2bd384eca29b07d57c912aef0436807c50636
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-20 16:08:30 +0530

    Update task.js

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,11 +17,6 @@ router.post('/tasks', auth, async (req, res) => {
         res.status(400).send(e);
     }
 
-    // task.save().then(() => {
-    //     res.status(201).send(task);
-    // }).catch((error) => {
-    //    res.status(400).send(error);
-    // });
 });
 
 //GET /tasks?completed=true
@@ -43,9 +38,7 @@ router.get('/tasks', auth, async (req, res) => {
     }
 
     try {
-    //    const tasks = await Task.find({});
-    //    const tasks = await Task.find({ owner: req.user._id });
-    // await req.user.populate('tasks').execPopulate();
+
     await req.user.populate({
         path: 'tasks',
         match,
@@ -64,11 +57,6 @@ router.get('/tasks', auth, async (req, res) => {
         res.status(500).send();
     }
     
-    // Task.find({}).then((tasks) => {
-    //     res.send(tasks);
-    // }).catch((e) => {
-    //     res.status(500).send();
-    // });
 });
 
 router.get('/tasks/:id', auth, async (req, res) => {
@@ -88,17 +76,6 @@ router.get('/tasks/:id', auth, async (req, res) => {
         res.status(500).send();
     }
 
-
-    // Task.findById(_id).then((task) => {
-    //     if (!task) {
-    //         return res.status(404).send();
-    //     }
-
-    //     res.send(task);
-
-    // }).catch((e) => {
-    //     res.status(500).send();
-    // });
 });
 
 router.patch('/tasks/:id', auth, async (req, res) => {
@@ -112,14 +89,8 @@ router.patch('/tasks/:id', auth, async (req, res) => {
         return res.status(400).send({ error: 'Invald Update..!'});
     }
 
-    try {
-        //const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true});
-        
+    try {   
         const task = await Task.findOne({ _id: req.params.id, owner: req.user._id});
-
-        // const task = await Task.findById(_id);
-
-        
 
         if (!task) {
             return res.status(400).send();
@@ -138,11 +109,8 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
 
-   // const _id = req.params.id;
 
     try {
-        //const task = await Task.findByIdAndDelete(_id);
-
         const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id});
 
         if (!task) {
