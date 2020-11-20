30ce4ae7a5296f01d214f54496e9e30dd0dda738
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-20 16:04:01 +0530

    Update account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,19 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-// const sendGridAPIKey = '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';
-
-// const sendGridAPIKey = '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';
-
-// sgMail.setApiKey(sendGridAPIKey);
-
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// sgMail.send({
-//     to: 'sujithacharya2014@gmail.com',
-//     from: 'sujithacharya2014@gmail.com',
-//     subject: 'This the my first creation..!',
-//     text: 'First try send mail from node.js using api sendgrid on the project Task-App'
-// });
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
